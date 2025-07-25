d574a37d7c4d5a4d3a1ef05a3d701b06348b48ce
Rpinto30 <rodrigopinto3007@gmail.com>
2025-07-25 09:30:24 -0600

    Creacion menu

--- acatividad_8.py
+++ acatividad_8.py
@@ -1,1 +1,11 @@
-print("asoijaddsjli")
+products = []
+
+while True:
+    print("-"*10+"Control de productos"+"-"*10)
+    print("1) Agregar producto\n2) Modificar prodcuto existente\n3) Eliminar producto\n4)Ver productos\n5) Salir")
+    op = input("Ingrese una opcion: ")
+    match op:
+        case '5':
+            print("\nHasta pronto!")
+            break
+        case _: print("\nLo siento, intente de nuevo")
